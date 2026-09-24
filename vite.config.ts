5a430521a5ad54bb21b1eccac5d588783a53f72a
bilalbases-beep <bilal.bases@gmail.com>
2026-09-24 15:58:33 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => ({
     host: "::",
     port: 8080,
   },
-  base: '/stupid_ai/',
+  base: '/stupid-ai/',
   plugins: [dyadComponentTagger(), react()],
   resolve: {
     alias: {
